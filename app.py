92b3dd5a78fa7d716a873f4ee32b49016116a604
markwst <Wongsathorn_W@gmail.com>
2023-01-14 00:18:46 +0700

    Add CORS and search by des

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify, make_response
 import search
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/searchbytitle', methods=['GET'])
 def animeSearch():
@@ -10,7 +11,19 @@ def animeSearch():
     result = search.searchByTitle(query_term).T
     jsonResult = result.to_json()
     response = make_response(jsonResult)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
     return response
+
+@app.route('/searchbydescription', methods=['GET'])
+def animedesSearch():
+    argList = request.args.to_dict(flat=False)
+    query_term = argList['query'][0]
+    result = search.searchByDescription(query_term).T
+    jsonResult = result.to_json()
+    response = make_response(jsonResult)
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- search.py
+++ search.py
@@ -1,5 +1,4 @@
 import string
-
 import numpy as np
 import pandas as pd
 from rank_bm25 import BM25Okapi
@@ -31,6 +30,31 @@ def searchByTitle(query):
 
     return data.iloc[rank[:relevent_document]]
 
+def searchByDescription(query):
+    data = pd.read_csv('src/resource/anime.csv')
+    Description = data['synopsis']
+    corpus = Description.to_numpy().tolist()
+    cleaned_corpus = []
+    for doc in corpus:
+        cleaned_doc = remove_puncts(doc, string)
+        cleaned_corpus.append(cleaned_doc)
+    tokenized_clean_corpus = []
+    # Ex. {[dragon ball]} -> {[dragon], [ball]}
+    for doc in cleaned_corpus:
+        doc = doc.split()
+        tokenized_clean_corpus.append(doc)
+    bm25 = BM25Okapi(tokenized_clean_corpus)
+    # Create txt file
+    # ac.trainTitleTextFile(ac.get_text(tokenized_clean_corpus))
+    relevent_document = 0
+    tokenized_query = remove_puncts(query, string).split(" ")
+    doc_scores = bm25.get_scores(tokenized_query).tolist()
+    for score in doc_scores:
+        if score != 0.0:
+            relevent_document += 1
+    rank = np.argsort(doc_scores)[::-1]
+
+    return data.iloc[rank[:relevent_document]]
     # delete special symbols +-*/!@#$
 
 
